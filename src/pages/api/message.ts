a3449c582af1e47fa73a8b9d813d793381cda695
Renzo Visperas <renzoviperas07@gmail.com>
2023-04-04 02:48:17 +0800

    listen to message real time firebase

--- src/pages/api/message.ts
+++ src/pages/api/message.ts
@@ -24,7 +24,7 @@ export default async function handler(
     const newMessage = {
       message,
       sender,
-      createdAt: Date.now(),
+      createdAt: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
     };
     const docRef = await messagesRef.add(newMessage);
     res.status(200).json({ success: true, data: docRef.id });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import { auth } from "@/firebase-config";
+import { auth, db } from "@/firebase-config";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useState } from "react";
+import { onSnapshot, collection, query } from "firebase/firestore";
 
 type FormData = {
   message: string;
+};
+
+type Message = {
+  createdAt: number;
+  message: string;
+  sender: string;
 };
 
 export default function Home() {
@@ -25,6 +32,19 @@ export default function Home() {
       }
     });
   }, [router]);
+
+  useEffect(() => {
+    const q = query(collection(db, "messages"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      const messages: Message[] = [];
+      querySnapshot.forEach((doc) => {
+        messages.push(doc.data() as Message);
+      });
+      console.log("Current messages: ", messages);
+    });
+
+    return () => unsub();
+  }, []);
 
   const handleLogout = async () => {
     await auth.signOut();
